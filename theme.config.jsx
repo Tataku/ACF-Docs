970492353be740e370a54f71e0442a74a0e30ca9
Tataku <13910651+Tataku@users.noreply.github.com>
2026-02-25 15:41:13 -0600

    Add files via upload

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  project: { link: "https://github.com/REPLACE_ME/ACF-Docs" },
+  docsRepositoryBase: "https://github.com/REPLACE_ME/ACF-Docs/blob/main",
+  titleSuffix: " – ACF Docs",
+  search: true,
+  footer: { text: "Adaptive Convexity Framework" }
+};
