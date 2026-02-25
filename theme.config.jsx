22507ae1519ff99b2f677568fc721fc6da6d8ba5
Tataku <13910651+Tataku@users.noreply.github.com>
2026-02-25 15:50:29 -0600

    Delete theme.config.jsx

--- theme.config.jsx
+++
@@ -1,7 +0,0 @@
-export default {
-  project: { link: "https://github.com/REPLACE_ME/ACF-Docs" },
-  docsRepositoryBase: "https://github.com/REPLACE_ME/ACF-Docs/blob/main",
-  titleSuffix: " – ACF Docs",
-  search: true,
-  footer: { text: "Adaptive Convexity Framework" }
-};
