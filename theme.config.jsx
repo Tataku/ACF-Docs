c58adf9855c615b72573af7c59c7c4c45fec49a9
Tataku <13910651+Tataku@users.noreply.github.com>
2026-02-25 15:32:22 -0600

    Update project links in theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,6 @@
 export default {
-  project: { link: "https://github.com/REPLACE_ME/ACF-Docs" },
-  docsRepositoryBase: "https://github.com/REPLACE_ME/ACF-Docs/blob/main",
+  project: { link: "https://github.com/Tataku/ACF-Docs" },
+  docsRepositoryBase: "https://github.com/Tataku/ACF-Docs/blob/main",
   titleSuffix: " – ACF Docs",
   search: true,
   footer: { text: "Adaptive Convexity Framework" }
